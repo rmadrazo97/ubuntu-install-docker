6bb101e3a247fd3eec5d2d7f6d253ce70cd07d2d
Alejandro Madrazo <jmadrazo7@gmail.com>
2021-04-26 11:34:51 -0600

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -12,6 +12,7 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo apt-get install docker-compose
 sudo docker run hello-world
 # Linux post-install
 sudo groupadd docker
